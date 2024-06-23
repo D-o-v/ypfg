cb5a990cf5810ca724f1d67b689a0f5f50b97501
odunayo_dauda <daudavictorodunayo@gmail.com>
2024-06-23 20:57:36 +0100

    Redirect added

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,7 +1,15 @@
 
 const Resources = () => {
   return (
-    <div className='min-h-screen p-4 lg:p-10'>Resources</div>
+    <div className='min-h-screen p-4 lg:p-10'>
+      
+      
+    <div className="container flex items-center justify-center">
+        <div className="relative w-[200px] h-[250px] rounded-[20px] bg-gradient-to-br from-[rgba(58,56,56,0.623)] to-[#1f1f1f] shadow-[0_25px_50px_rgba(0,0,0,0.55)] cursor-pointer transition-all duration-300 transform hover:scale-90">
+            <span className="absolute overflow-hidden w-[150px] h-[150px] top-[-10px] left-[-10px] flex items-center justify-center before:content-['Premium'] before:absolute before:w-[150%] before:h-[40px] before:bg-gradient-to-r before:from-[#ff6547] before:via-[#ffb144] before:to-[#ff7053] before:rotate-[-45deg] before:translate-y-[-20px] before:flex before:items-center before:justify-center before:text-white before:font-semibold before:tracking-[0.1em] before:uppercase before:shadow-[0_5px_10px_rgba(0,0,0,0.23)] after:content-[''] after:absolute after:w-[10px] after:h-[10px] after:bottom-0 after:left-0 after:box-shadow-[140px_-140px_#cc3f47] after:bg-gradient-to-r after:from-[#FF512F] after:via-[#F09819] after:to-[#FF512F]"></span>
+        </div>
+</div>
+    </div>
   )
 }
 
